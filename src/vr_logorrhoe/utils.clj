01777dda9060b61a16db976f79d25083cea312c4
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2016-04-29 14:09:49 +0200

    Add helper function to print an InputStream to the REPL

--- src/vr_logorrhoe/utils.clj
+++ src/vr_logorrhoe/utils.clj
@@ -5,6 +5,21 @@
   (map #(.toString %)
        (seq (.getDeclaredMethods (.getClass obj)))))
 
+(defn print-input-stream [input-stream]
+  "Helper function to print an InputStream to the REPL"
+  (prn "Printing input-stream: " input-stream)
+  (try
+    (let [in (new BufferedReader (new InputStreamReader input-stream))]
+      (loop []
+        (let [line (.readLine in)]
+          ;; (when (> (count line))
+            (prn line))
+            ;; (recur)))))
+    (.close input-stream)))
+    (catch Exception e
+      (println "Caught: " e))))
+;; (print-input-stream (new java.io.FileInputStream "/tmp/foo"))
+
 ;; (defn crazy-batshit-from-c [num]
 ;;   (jna/invoke Integer c/printf "The number given: %d\n" num))
 ;; (crazy-batshit-from-c 112423)
