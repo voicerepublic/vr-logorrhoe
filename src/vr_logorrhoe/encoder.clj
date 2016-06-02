7721be62b683fbcd162313fd9d62a64047ee658e
Munen Alain M. Lafon <munen@dispatched.ch>
2016-06-02 15:33:28 +0200

    add todo for encoding

--- src/vr_logorrhoe/encoder.clj
+++ src/vr_logorrhoe/encoder.clj
@@ -4,6 +4,7 @@
 (defn encode [input callback]
   "Encodes an input-stream using `lame` and pipes the result into the
   `callback` function"
+  ;; TODO: Read frequency and bitwidth from @config
   (sh "lame" "-r" "--cbr" "-b" "256" "-s" "44.1" "--bitwidth" "16" "--signed" "--little-endian" "-m" "m" "-" "-"
       :in input
       :err #(do (prn "lame has written to stderr!")
