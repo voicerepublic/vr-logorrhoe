f8ad6f7ed402c76265c34d84219ec0f995c224c6
phil <phil@branch14.org>
2016-06-21 09:54:24 +0200

    initial (defunct) draft of registering

--- src/vr_logorrhoe/vrapi.clj
+++ src/vr_logorrhoe/vrapi.clj
@@ -1,21 +1,48 @@
 (ns vr-logorrhoe.vrapi
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+            [cheshire.core :as json]))
 
 
+;; TODO move to utils
 (defn uuid []
   (str (java.util.UUID/randomUUID)))
 
 ;; TODO needs to come from settings
 (def identifier (uuid))
 
+;; ------------------------------ knocking
+
 (def default-endpoint
-  "https://voicerepublic.com/api/devices")
+  ;;"https://voicerepublic.com/api/devices")
+  "http://localhost:3000/api/devices")
 
 (defn knock []
   (let [data (-> (str default-endpoint "/" identifier)
-                 client/get
+                 (client/get {:accept :json})
                  :body
-                 (json/read-str :key-fn keyword))]
+                 (json/parse-string true))]
     ;; TOOD needs to be stored in settings
+    (prn data)))
+
+;; ------------------------------ registering
+
+(defn- register-payload []
+  {:device
+   {:identifier identifier
+    :type "vr-restream" ;; TODO use a name also used elsewhere
+    :subtype "0"}}) ;; TODO use a version/buildnumber also used elsewhere
+
+(defn- register-options []
+  {:form-params (register-payload)
+   :content-type :json
+   :accept :json
+   :debug true
+   :throw-entire-message? true})
+
+(defn register []
+  ;; TODO use enpoint from settings instead of default-endpoint
+  (let [data (-> (client/post default-endpoint (register-options))
+                 :body
+                 (json/parse-string true))]
+    ;; TODO needs to be stored in settings
     (prn data)))
